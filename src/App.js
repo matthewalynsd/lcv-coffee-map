afe6d9dab0dde44220d84366a12de72cdaa7595c
matthewalynsd <masd32@gmail.com>
2022-03-12 22:51:00 -0800

    Cleanup errors and prep for deploy

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React, { useEffect } from 'react';
-import './App.css';
-import { coffeeData } from './coffee-data';
-import L, { map } from 'leaflet';
+import React, { useEffect, useRef } from "react";
+import "./App.css";
+import { coffeeData } from "./coffee-data";
+import L from "leaflet";
 
 function App() {
   return (
@@ -9,83 +9,101 @@ function App() {
       <div id="menu">
         <h1>Coffee Tracker v1</h1>
         <MenuItems />
+      </div>
+      <LeafletMap />
     </div>
-    <LeafletMap />
-  </div>
   );
 }
 
-function MenuItems(){
-  return(
+function MenuItems() {
+  return (
     <>
-      {coffeeData.map(({ title, address, latLong, driveThrough, dineIn, roaster}) => (
-        <div key={title} className="menu-item">
-          <h2>{title}</h2>
+      {coffeeData.map(
+        ({ title, address, latLong, driveThrough, dineIn, roaster }) => (
+          <div key={title} className="menu-item">
+            <h2>{title}</h2>
             <p>{address}</p>
             <div className="icon-row">
-              {driveThrough === true &&
+              {driveThrough === true && (
                 <i className="fa-solid fa-car" title="Drive Through"></i>
-              }
-              {dineIn === true &&
+              )}
+              {dineIn === true && (
                 <i className="fa-solid fa-utensils" title="Dine In"></i>
-              }
+              )}
             </div>
-        </div>
-      ))}
+          </div>
+        )
+      )}
     </>
   );
 }
 
-export function LeafletMap(){
-  let map = null;
+export function LeafletMap() {
+  const map = useRef(null);
+
   useEffect(() => {
-    map = L.map("map").setView([46.406329, -117.038663], 14);
-    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    minZoom: 13,
-    id: 'mapbox/dark-v10',
-    tileSize: 512,
-    zoomOffset: -1,
-    accessToken: 'pk.eyJ1IjoibWF0dGhld2FseW5kIiwiYSI6ImNqdnNvcWQ3cDM4MWY0M3FvdGc1YnF2OXAifQ.1GIr-xDXI-8SPEuZMVB_ug'
-    }).addTo(map); 
-  
+    map.current = L.map("map").setView([46.406329, -117.038663], 14);
+    L.tileLayer(
+      "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
+      {
+        attribution:
+          'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        maxZoom: 18,
+        minZoom: 13,
+        id: "mapbox/dark-v10",
+        tileSize: 512,
+        zoomOffset: -1,
+        accessToken:
+          "pk.eyJ1IjoibWF0dGhld2FseW5kIiwiYSI6ImNqdnNvcWQ3cDM4MWY0M3FvdGc1YnF2OXAifQ.1GIr-xDXI-8SPEuZMVB_ug",
+      }
+    ).addTo(map.current);
+
     coffeeData.forEach(function (coffeeDataItem) {
-      const splitCoords = coffeeDataItem.latLong.split(', ');
-      const lat =  splitCoords[0];
+      const splitCoords = coffeeDataItem.latLong.split(", ");
+      const lat = splitCoords[0];
       const lon = splitCoords[1];
 
       var defaultIcon = L.icon({
-        iconUrl: 'custom-icon.png',
+        iconUrl: "custom-icon.png",
         iconSize: [32, 32],
         tooltipAnchor: [1, -16],
-        iconAnchor: [16, 30]
+        iconAnchor: [16, 30],
       });
       var hoverIcon = L.icon({
-        iconUrl: 'custom-icon-hover.png',
+        iconUrl: "custom-icon-hover.png",
         iconSize: [32, 32],
         tooltipAnchor: [1, -16],
-        iconAnchor: [16, 30]
+        iconAnchor: [16, 30],
       });
 
-      var mapMarker = L.marker([ lat, lon ], {icon: defaultIcon}).addTo(map);
+      var mapMarker = L.marker([lat, lon], { icon: defaultIcon }).addTo(
+        map.current
+      );
       mapMarker.bindTooltip(coffeeDataItem.title);
 
-      mapMarker.on('mouseover', function(e) {
+      mapMarker.on("mouseover", function (e) {
         mapMarker.setIcon(hoverIcon);
         mapMarker.openTooltip();
       });
-      mapMarker.on('mouseout', function(e) {
+      mapMarker.on("mouseout", function (e) {
         mapMarker.setIcon(defaultIcon);
         mapMarker.closeTooltip();
       });
     });
-  },[])
+  }, []);
 
-  return <div id="map">
-    <button className="recenter-button"
-      onClick={() => {map.setView([46.406329, -117.038663], 14);}}>Re-Center</button>
-  </div>;
+  return (
+    <div id="map">
+      <button
+        className="recenter-button"
+        onClick={() => {
+          map.current.setView([46.406329, -117.038663], 14);
+        }}
+      >
+        Re-Center
+      </button>
+    </div>
+  );
 }
 
 export default App;
