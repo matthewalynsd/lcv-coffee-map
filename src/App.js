e07682f50aa6e77a5a9207a5b87f930d2824129b
matthewalynsd <48599575+matthewalynsd@users.noreply.github.com>
2022-03-14 12:38:55 -0700

    Switch from ternary to classList.toggle()

--- src/App.js
+++ src/App.js
@@ -51,10 +51,10 @@ function handleMenuClick(){
    const menu = document.getElementById('menu');
    const map = document.getElementById('map');
    const menuIcon = document.getElementById('menuIcon');
-   menu.classList.contains('active') ? menu.classList.remove('active') : menu.classList.add('active');
-   map.classList.contains('inactive') ? map.classList.remove('inactive') : map.classList.add('inactive');
-   menuIcon.classList.contains('fa-bars') ? menuIcon.classList.remove('fa-bars') : menuIcon.classList.add('fa-bars');
-   menuIcon.classList.contains('fa-xmark') ? menuIcon.classList.remove('fa-xmark') : menuIcon.classList.add('fa-xmark');
+   menu.classList.toggle('active');
+   map.classList.toggle('inactive');
+   menuIcon.classList.toggle('fa-bars');
+   menuIcon.classList.toggle('fa-xmark');
 };
 
 export default App;
