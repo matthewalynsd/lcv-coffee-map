afc28f671098a1d87ca776e983a2a41dccd6ac11
matthewalynsd <48599575+matthewalynsd@users.noreply.github.com>
2022-03-14 09:38:23 -0700

    Moved Leaflet Map to separate file

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useRef } from "react";
 import "./App.css";
+import LeafletMap from "./components/LeafletMap.js";
 import { coffeeData } from "./coffee-data";
 import L from "leaflet";
 
@@ -47,70 +48,5 @@ function handleMenuClick(){
    menuIcon.classList.contains('fa-bars') ? menuIcon.classList.remove('fa-bars') : menuIcon.classList.add('fa-bars');
    menuIcon.classList.contains('fa-xmark') ? menuIcon.classList.remove('fa-xmark') : menuIcon.classList.add('fa-xmark');
 };
-
-export function LeafletMap() {
-  const map = useRef(null);
-
-  useEffect(() => {
-    map.current = L.map("map").setView([46.406329, -117.038663], 14);
-    L.tileLayer(
-      "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
-      {
-        attribution:
-          'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-        maxZoom: 18,
-        minZoom: 13,
-        id: "mapbox/dark-v10",
-        tileSize: 512,
-        zoomOffset: -1,
-        accessToken:
-          "pk.eyJ1IjoibWF0dGhld2FseW5kIiwiYSI6ImNqdnNvcWQ3cDM4MWY0M3FvdGc1YnF2OXAifQ.1GIr-xDXI-8SPEuZMVB_ug",
-      }
-    ).addTo(map.current);
-
-    coffeeData.forEach(function (coffeeDataItem) {
-      const splitCoords = coffeeDataItem.latLong.split(", ");
-      const lat = splitCoords[0];
-      const lon = splitCoords[1];
-
-      var defaultIcon = L.icon({
-        iconUrl: "custom-icon.png",
-        iconSize: [32, 32],
-        tooltipAnchor: [1, -16],
-        iconAnchor: [16, 30],
-      });
-
-      var hoverIcon = L.icon({
-        iconUrl: "custom-icon-hover.png",
-        iconSize: [32, 32],
-        tooltipAnchor: [1, -16],
-        iconAnchor: [16, 30],
-      });
-
-      var mapMarker = L.marker([lat, lon], { icon: defaultIcon }).addTo(
-        map.current
-      );
-      mapMarker.bindTooltip(coffeeDataItem.title);
-
-      mapMarker.on("mouseover", function (e) {
-        mapMarker.setIcon(hoverIcon);
-        mapMarker.openTooltip();
-      });
-
-      mapMarker.on("mouseout", function (e) {
-        mapMarker.setIcon(defaultIcon);
-        mapMarker.closeTooltip();
-      });
-    });
-  }, []);
-
-  return (
-    <div id="map">
-      <div className="menu-icon">
-        <i className="fa fa-bars" onClick={handleMenuClick} id='menuIcon'></i>
-      </div>
-    </div>
-  );
-}
 
 export default App;
