302c4a4cc9cb9aca82f05e3e3980b17de0447eb7
matthewalynsd <48599575+matthewalynsd@users.noreply.github.com>
2022-03-14 15:13:34 -0700

    Update LeafletMap.js

--- src/components/LeafletMap.js
+++ src/components/LeafletMap.js
@@ -3,81 +3,61 @@ import { coffeeData } from "../coffee-data";
 import L from "leaflet";
 
 export default function LeafletMap() {
-  const mapRef = useRef();
-  
+  const map = useRef(null);
+
+  function handleOnLocationFound(event)
+  {
+    const latlng = event.latlng;
+    const radius = event.accuracy;
+    const circle = L.circle(latlng, radius);
+    circle.addTo(map.current);
+  }
   useEffect(() => {
-    const { current = {} } = mapRef;
-    const { leafletElement: map } = current;
-    
     map.current = L.map("map").setView([46.406329, -117.038663], 14);
     L.tileLayer(
-      "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
-      {
-        attribution:
-          'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-        maxZoom: 18,
-        minZoom: 13,
-        id: "mapbox/dark-v10",
-        tileSize: 512,
-        zoomOffset: -1,
-        accessToken:
-          "pk.eyJ1IjoibWF0dGhld2FseW5kIiwiYSI6ImNqdnNvcWQ3cDM4MWY0M3FvdGc1YnF2OXAifQ.1GIr-xDXI-8SPEuZMVB_ug",
-      }
-    ).addTo(map);
-    
-    map.locate({
+@@ -26,14 +33,8 @@ export default function LeafletMap() {
       setView: true
     });
-    
-    map.on('locationFound', handleOnLocationFound);
-    
+
+    function handleOnLocationFound(event)
+    {
+      const latlng = event.latlng;
+      const radius = event.accuracy;
+      const circle = L.circle(latlng, radius);
+      circle.addTo(map.current);
+    }
+    map.current.on('locationFound', handleOnLocationFound);
+
     coffeeData.forEach(function (coffeeDataItem) {
       const splitCoords = coffeeDataItem.latLong.split(", ");
       const lat = splitCoords[0];
       const lon = splitCoords[1];
-
       var defaultIcon = L.icon({
         iconUrl: "custom-icon.png",
         iconSize: [32, 32],
         tooltipAnchor: [1, -16],
         iconAnchor: [16, 30],
       });
-
       var hoverIcon = L.icon({
         iconUrl: "custom-icon-hover.png",
         iconSize: [32, 32],
         tooltipAnchor: [1, -16],
         iconAnchor: [16, 30],
       });
-
       var mapMarker = L.marker([lat, lon], { icon: defaultIcon }).addTo(
-        map
+        map.current
       );
       mapMarker.bindTooltip(coffeeDataItem.title);
-
       mapMarker.on("mouseover", function (e) {
         mapMarker.setIcon(hoverIcon);
         mapMarker.openTooltip();
       });
-
       mapMarker.on("mouseout", function (e) {
         mapMarker.setIcon(defaultIcon);
         mapMarker.closeTooltip();
       });
     });
   }, []);
-
-  function handleOnLocationFound(event)
-  {
-    const { current = {} } = mapRef;
-    const { leafletElement: map } = current;
-    
-    const latlng = event.latlng;
-    const radius = event.accuracy;
-    const circle = L.circle(latlng, radius);
-    circle.addTo(map);
-  }
-  
   return (
     <div id="map"></div>
   );
